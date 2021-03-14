ed3f6294bd6a488369c2cefe9237c9499d6249be
paalamugan <paalamugan@gmail.com>
2021-03-14 18:29:40 +0530

    removed public folder

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(require('express-validator')());
 app.use(cookieParser());
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'client/build')));
 
 app.use('/api/payslip', upload.single('companyIcon'), payslipRouter);
 
