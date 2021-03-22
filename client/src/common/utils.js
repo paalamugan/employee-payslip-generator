5a6649c18a73bed96c608ca0997620ed02510c3f
paalamugan <paalamugan@gmail.com>
2021-03-22 18:02:24 +0530

    Changed text

--- client/src/common/utils.js
+++ client/src/common/utils.js
@@ -12,7 +12,7 @@ export const PayslipSampleData = {
   company: {
     icon: null,
     iconUrl: '',
-    name: 'YesVision Pvt ltd.',
+    name: 'YesVision pvt ltd.',
     address: '250, S-BLock, 27 Street, Adayar, Chennai: 600027'
   },
   employee: {
