be606344280bc998e95d45a36c8563ca91a0fb1e
paalamugan <paalamugan@gmail.com>
2022-09-19 04:48:37 +0530

    chore: remove locale support

--- email-templates/index.js
+++ email-templates/index.js
@@ -1,30 +1,30 @@
-import path from 'path';
-import Email from 'email-templates';
+import path from "path";
+import Email from "email-templates";
 // import { minify } from 'html-minifier';
-import utils from './utils';
-import templateData from './template-data';
+import utils from "./utils";
+import templateData from "./template-data";
 
 const email = new Email({
-    views: {
-        root: path.join(__dirname, 'templates')
+  views: {
+    root: path.join(__dirname, "templates"),
+  },
+  juice: true,
+  juiceResources: {
+    preserveImportant: true,
+    webResources: {
+      relativeTo: path.join(__dirname, "templates"),
     },
-    juice: true,
-    juiceResources: {
-        preserveImportant: true,
-        webResources: {
-            relativeTo: path.join(__dirname, 'templates')
-        }
-    },
-    i18n: {},
+  },
+  // i18n: {},
 
-    // message: {
-    //     from: 'no-reply@gmail.com'
-    // },
-    // // uncomment below to send emails in development/test env:
-    // // send: true,
-    // transport: {
-    //     jsonTransport: true
-    // }
+  // message: {
+  //     from: 'no-reply@gmail.com'
+  // },
+  // // uncomment below to send emails in development/test env:
+  // // send: true,
+  // transport: {
+  //     jsonTransport: true
+  // }
 });
 
 // const minifyOptions = {
@@ -40,35 +40,28 @@ const email = new Email({
 // };
 
 const renderEmailTemplate = async (templateName, data) => {
+  if (typeof templateName !== "string") {
+    throw new Error("First parameter must be a string!");
+  }
 
-    if (typeof templateName !== "string") {
-        throw new Error("First parameter must be a string!");
-    }
+  let localData = templateData[templateName];
+  let defaultData = utils.defaultTemplateData();
 
-    let localData = templateData[templateName];
-    let defaultData = utils.defaultTemplateData();
+  if (!localData) {
+    throw new Error(
+      `Invalid template name: ${templateName}!. template name must be mention inside template-data.js file.`
+    );
+  }
 
-    if (!localData) {
-        throw new Error(`Invalid template name: ${templateName}!. template name must be mention inside template-data.js file.`);
-    }
+  Object.assign(localData, data, defaultData);
 
-    Object.assign(localData, data, defaultData);
-
-    try {
-        let html = await email.render(templateName, localData);
-        // html = minify(html, minifyOptions);
-        return html;
-    } catch (err) {
-        throw err;
-    }
+  try {
+    let html = await email.render(templateName, localData);
+    // html = minify(html, minifyOptions);
+    return html;
+  } catch (err) {
+    throw err;
+  }
 };
 
 export default renderEmailTemplate;
-
-
-
-
-
-
-
-
